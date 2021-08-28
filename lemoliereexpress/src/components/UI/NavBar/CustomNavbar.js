0fa2903793d9c8bf35331c980b463f5875290ab7
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2021-08-28 20:50:39 +0200

    link to home in navbar

--- lemoliereexpress/src/components/UI/NavBar/CustomNavbar.js
+++ lemoliereexpress/src/components/UI/NavBar/CustomNavbar.js
@@ -10,7 +10,7 @@ import { useState } from "react";
 
 import { useTranslation } from "react-i18next";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const CustomNavbar = () => {
   const [navExpanded, setNavExpanded] = useState(false);
@@ -20,7 +20,7 @@ const CustomNavbar = () => {
   return (
     <Navbar sticky="top" bg="light" variant="light" expand="xl" className={classes.navbar} expanded={navExpanded}>
       <Container>
-        <Navbar.Brand style={{fontSize: "2rem"}}>Le Molière Express</Navbar.Brand>
+        <Navbar.Brand style={{fontSize: "2rem"}}><NavLink to="/home" className={classes.title}>Le Molière Express</NavLink></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => navExpanded ? setNavExpanded(false) : setNavExpanded(true)}/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
